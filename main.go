7aecbe69a53a16ffde62241987eb3b9ffe623c68
SAWADA Tadashi <cesare@mayverse.jp>
2025-11-06 16:55:50 +0900

    Parse command line arguments

--- main.go
+++ main.go
@@ -1,4 +1,24 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+type arguments struct {
+	configPath string
+}
+
+func newArguments() *arguments {
+	var configPath string
+	flag.StringVar(&configPath, "config-path", "azarole.toml", "specify path to configuration file")
+	flag.Parse()
+
+	return &arguments{configPath: configPath}
+}
+
 func main() {
+	args := newArguments()
+	fmt.Fprintf(os.Stdout, "configPath: %s\n", args.configPath)
 }
