99d5b788bc97c44463feca9d6b7f9b3b6cd3b077
SAWADA Tadashi <cesare@mayverse.jp>
2025-11-08 11:45:21 +0900

    Enable sessions and cors

--- internal/server.go
+++ internal/server.go
@@ -1,8 +1,26 @@
 package app
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-gonic/gin"
+)
 
 func Engine(config *Config, secrets *Secrets) (*gin.Engine, error) {
 	engine := gin.Default()
+
+	store := cookie.NewStore(secrets.Session.SessionKey.Bytes())
+	engine.Use(sessions.Sessions("azarole-session", store))
+
+	engine.Use(cors.New(cors.Config{
+		AllowOrigins: []string{config.Frontend.BaseUrl},
+		AllowMethods: []string{"DELETE", "GET", "OPTIONS", "POST"},
+		AllowHeaders: []string{
+			"Content-Type",
+		},
+		AllowCredentials: true,
+	}))
+
 	return engine, nil
 }
