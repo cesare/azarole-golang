8ed14c4dba0c9f5475023e0556987c6b3a73b323
SAWADA Tadashi <cesare@mayverse.jp>
2025-11-17 11:13:25 +0900

    Enable to clock-in/clock-out

--- internal/handlers/api/attendance_records.go
+++ internal/handlers/api/attendance_records.go
@@ -3,20 +3,78 @@ package api
 import (
 	"azarole/internal/core"
 	"azarole/internal/models"
+	"azarole/internal/resources"
+	"azarole/internal/views"
+	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 func RegisterAttendanceRecordsHandlers(group *gin.RouterGroup, app *core.App) {
 	group.POST("/workplaces/:workplace_id/clock_ins", func(c *gin.Context) {
-		create(c, models.ClockIn)
+		create(c, app, models.ClockIn)
 	})
 	group.POST("/workplaces/:workplace_id/clock_outs", func(c *gin.Context) {
-		create(c, models.ClockOut)
+		create(c, app, models.ClockOut)
 	})
 }
 
-func create(c *gin.Context, event models.AttendanceEvent) {
-	c.Status(http.StatusCreated)
+func create(c *gin.Context, app *core.App, event models.AttendanceEvent) {
+	currentUser := c.MustGet("currentUser").(models.User)
+
+	v := c.Param("workplace_id")
+	workplaceId, err := models.FromStringToWorkplaceId(v)
+	if err != nil {
+		c.Status(http.StatusNotFound)
+		return
+	}
+
+	wrs := resources.NewWorkplaceResources(app, &currentUser)
+	workplace, err := wrs.Find(workplaceId)
+	if err != nil {
+		c.Status(http.StatusInternalServerError)
+		return
+	}
+	if workplace == nil {
+		c.Status(http.StatusNotFound)
+		return
+	}
+
+	attendance, err := createAttendance(app, workplace, event)
+	if err != nil {
+		c.Status(http.StatusInternalServerError)
+		return
+	}
+
+	view := views.FromAttendanceRecord(attendance)
+	c.JSON(http.StatusCreated, gin.H{
+		"attendanceRecord": view,
+	})
+}
+
+func createAttendance(app *core.App, workplace *models.Workplace, event models.AttendanceEvent) (*models.AttendandeRecord, error) {
+	statement, err := app.Database().Prepare("insert into attendance_records (workplace_id, event, recorded_at, created_at) values ($1, $2, $3, $4) returning id, workplace_id, event, recorded_at")
+	if err != nil {
+		return nil, fmt.Errorf("failed to prepare statement for findWorkplaces: %s", err)
+	}
+	defer statement.Close()
+
+	now := time.Now().UTC()
+	rows, err := statement.Query(workplace.Id, event, now, now)
+	if err != nil {
+		return nil, fmt.Errorf("query for creating attandance record failed: %s", err)
+	}
+	defer rows.Close()
+
+	rows.Next()
+
+	var ar models.AttendandeRecord
+	err = rows.Scan(&ar.Id, &ar.WorkplaceId, &ar.Event, &ar.RecordedAt)
+	if err != nil {
+		return nil, fmt.Errorf("failed to map row into attendanceRecord: %s", err)
+	}
+
+	return &ar, nil
 }
--- internal/models/workplace.go
+++ internal/models/workplace.go
@@ -1,9 +1,24 @@
 package models
 
+import (
+	"fmt"
+	"strconv"
+)
+
 type WorkplaceId uint32
 
 type Workplace struct {
 	Id     WorkplaceId `db:"id"`
 	UserId UserId      `db:"user_id"`
 	Name   string      `db:"name"`
+}
+
+func FromStringToWorkplaceId(value string) (WorkplaceId, error) {
+	uintValue, err := strconv.ParseUint(value, 10, 32)
+	if err != nil {
+		dummy := WorkplaceId(0)
+		return dummy, fmt.Errorf("invalid workplace id string %s: %s", value, err)
+	}
+
+	return WorkplaceId(uintValue), nil
 }
